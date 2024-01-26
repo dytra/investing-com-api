c766ef38f9b6cd5ba9b793c184ce8b50a79558ea
dytra <vancielnugraha@gmail.com>
2024-01-27 01:40:14 +0700

    add 10 idx30 stocks

--- idxMapping.js
+++ idxMapping.js
@@ -1,7 +1,153 @@
 exports.default = {
   'equities/bbca': {
     pairId:'101354',
+    title:'Bank Central Asia (BBCA)',
+    name:'Bank Central Asia',
+  },
+  'equities/adro': {
+    pairId:'101281',
+    title:'Adaro Energy Tbk (ADRO)',
+    name:'Adaro Energy Tbk',
+  },
+  'equities/akra': {
+    pairId:'101289',
+    title:'Akr Corporindo Tbk (AKRA)',
+    name:'Akr Corporindo Tbk',
+  },
+  'equities/amrt': {
+    pairId:'101591',
+    title:'Sumber Alfaria Trijaya Tbk (AMRT)',
+    name:'Sumber Alfaria Trijaya Tbk',
+  },
+  'equities/antm': {
+    pairId:'101294',
+    title:'Aneka Tambang Persero Tbk (ANTM)',
+    name:'Aneka Tambang Persero Tbk',
+  },
+  'equities/asii': {
+    pairId:'101307',
+    title:'Astra International Tbk (ASII)',
+    name:'Astra International Tbk',
+  },
+  'equities/bbni': {
+    pairId:'101317',
+    title:'Bank Negara Indonesia Tbk (BBNI)',
+    name:'Bank Negara Indonesia Tbk',
+  },
+  'equities/bbri': {
+    pairId:'101336',
+    title:'Bank Rakyat Indonesia Persero (BBRI)',
+    name:'Bank Rakyat Indonesia Persero',
+  },
+  'equities/bmri': {
+    pairId:'101325',
+    title:'Bank Mandiri Persero Tbk PT (BMRI)',
+    name:'Bank Mandiri Persero Tbk PT',
+  },
+  'equities/bbca': {
+    pairId:'101354',
     title:'Bank Central Asia',
     name:'Bank Central Asia',
-  }
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  }, 
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+  'equities/bbca': {
+    pairId:'101354',
+    title:'Bank Central Asia',
+    name:'Bank Central Asia',
+  },
+
 }
